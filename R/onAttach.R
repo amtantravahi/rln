28876496123ad48514c7be8808ff2230ca75e1d3
Adi Tantravahi <amtantravahi@gmail.com>
2022-06-23 14:23:43 -0400

    update

--- R/onAttach.R
+++
@@ -1,1 +0,0 @@
-.onAttach(print("Adityamohan Tantravahi. Rln: R Package to Extract, Transform, and Load News Data from the Lexis Nexis Web Services API (version 0.1.0). R, 2022. https://github.com/amtantravahi/rln."))
---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname){
+  print(cat("Please Cite:
+
+        Tantravahi A (2022). rln: R Package for Lexis Nexis Web Services API. R package (version 0.1.0.)"))
+
+}
+
+
+.onAttach <- function(libname, pkgname){
+  print(cat("Please Cite:
+
+        Tantravahi A (2022). rln: R Package for Lexis Nexis Web Services API. R package (version 0.1.0.)"))
+}
