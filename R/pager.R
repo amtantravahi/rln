3725060c77ad0a7c4555a92f7094e12e5dc0c104
Adi Tantravahi <amtantravahi@gmail.com>
2022-06-22 21:34:41 -0400

    Update pager.R

--- R/pager.R
+++ R/pager.R
@@ -5,7 +5,7 @@
 # result$Document.Content variable we extracted from the API
 pager <- function(x) {
 
-  `%>%` <- magrittr::`%>%`
+  `%>% <- magrittr::`%>%`
 
 
   # Parse data as XML
@@ -15,35 +15,35 @@ pager <- function(x) {
   doc_html <- xml2::read_html(x)
 
   # Extract data into a seperate dataframe
-  data.frame(title = doc %>%
-               xml2::xml_find_one(xpath = "//nitf:hedline") %>%
+  data.frame(title = doc `%>%`
+               xml2::xml_find_one(xpath = "//nitf:hedline") `%>%`
                rvest::html_text(),
-             pub_date = doc_html %>%
-               rvest::html_node("published") %>%
+             pub_date = doc_html `%>%`
+               rvest::html_node("published") `%>%`
                rvest::html_text(),
-             author = doc_html %>%
-               rvest::html_node("author") %>%
+             author = doc_html `%>%`
+               rvest::html_node("author") `%>%`
                rvest::html_text(),
-             dateline = doc %>%
-               xml2::xml_find_one(xpath = "//nitf:dateline") %>%
+             dateline = doc `%>%`
+               xml2::xml_find_one(xpath = "//nitf:dateline") `%>%`
                rvest::html_text(),
-             main_text = doc %>%
-               xml2::xml_find_one(xpath = "//bodyText") %>%
+             main_text = doc `%>%`
+               xml2::xml_find_one(xpath = "//bodyText") `%>%`
                rvest::html_text(),
-             publisher = doc %>%
-               xml2::xml_find_one(xpath = "//publicationName") %>%
+             publisher = doc `%>%`
+               xml2::xml_find_one(xpath = "//publicationName") `%>%`
                rvest::html_text(),
-             pub_date_txt = doc %>%
-               xml2::xml_find_one(xpath = "//dateText") %>%
+             pub_date_txt = doc `%>%`
+               xml2::xml_find_one(xpath = "//dateText") `%>%`
                rvest::html_text(),
-             copyright = doc %>%
-               xml2::xml_find_one(xpath ="//copyright") %>%
+             copyright = doc `%>%`
+               xml2::xml_find_one(xpath ="//copyright") `%>%`
                rvest::html_text(),
-             lexis_id = doc_html %>%
-               rvest::html_node("id") %>%
+             lexis_id = doc_html `%>%`
+               rvest::html_node("id") `%>%`
                rvest::html_text(),
-             keyword_ln = doc %>%
-               xml2::xml_find_all("//className") %>%
+             keyword_ln = doc `%>%`
+               xml2::xml_find_all("//className") `%>%`
                xml2::xml_text()
   )
 
